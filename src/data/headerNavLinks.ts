b99cdf11d213e61172b3feb1741222fdc1ca8085
Naufal Akbar <kbrnugroho@gmail.com>
2023-06-13 15:30:01 +0700

    chnage nav link title

--- src/data/headerNavLinks.ts
+++ src/data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 export const headerNavLinks = [
   { title: 'Beranda', path: '/' },
   { title: 'Paket Wisata', path: '/tour-packages' },
-  { title: 'Kustomisasi Perjalanan', path: '/custom-trip' },
+  { title: 'Kustom Perjalanan', path: '/custom-trip' },
   { title: 'Rental Mobil', path: '/car-rental' },
   { title: 'Galeri', path: '/gallery' },
   { title: 'Tentang Kami', path: '/about-us' },
