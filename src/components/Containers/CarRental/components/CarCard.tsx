680e7f5db91fbf520c5904c850155f95f61641bd
Naufal Akbar <kbrnugroho@gmail.com>
2023-06-13 15:28:47 +0700

    fix bug visual

--- src/components/Containers/CarRental/components/CarCard.tsx
+++ src/components/Containers/CarRental/components/CarCard.tsx
@@ -486,72 +486,72 @@ const CarCard: FC = () => {
                 </div>
               </div>
             </div>
-          </div>
 
-          <div className="px-6 pt-1 pb-5">
-            <Link
-              href="/car-rental?product=halo#form-rental"
-              onClick={hrefTransition}
-              legacyBehavior
-            >
-              <a className="bg-teal-500 text-white font-bold py-2 px-4 rounded-lg">
-                Sewa Armada
-              </a>
-            </Link>
-          </div>
-        </div>
-
-        <div className="rounded-lg overflow-hidden shadow-md">
-          <img
-            className="w-full h-[350px] object-cover object-center"
-            src="https://images.unsplash.com/photo-1552374196-1ab2a1c593e8?auto=format&q=75&fit=crop&crop=top&w=600&h=700"
-            loading="lazy"
-            alt="Sunset in the mountains"
-          />
-
-          <div className="px-6 py-4">
-            <h2 className="font-bold text-xl text-primary">AVANZA</h2>
-
-            <p className="font-semibold text-lg text-gray-500">
-              IDR 150.000/hari
-            </p>
-
-            <div className="pt-3">
-              <div className="flex items-center pb-1">
-                <span className="mr-2 text-lg text-teal-500">
-                  <FaCheckCircle />
-                </span>
-                <p className="font-sans text-sm text-[#4F4F4F]">Driver</p>
-              </div>
-
-              <div className="flex items-center pb-1">
-                <span className="mr-2 text-lg text-teal-500">
-                  <FaCheckCircle />
-                </span>
-                <p className="font-sans text-sm text-[#4F4F4F]">BBM</p>
-              </div>
-
-              <div className="flex items-center">
-                <span className="mr-2 text-lg text-teal-500">
-                  <FaCheckCircle />
-                </span>
-                <p className="font-sans text-sm text-[#4F4F4F]">
-                  6-7 Penumpang
-                </p>
-              </div>
+            <div className="px-6 pt-1 pb-5">
+              <Link
+                href="/car-rental?product=halo#form-rental"
+                onClick={hrefTransition}
+                legacyBehavior
+              >
+                <a className="bg-teal-500 text-white font-bold py-2 px-4 rounded-lg">
+                  Sewa Armada
+                </a>
+              </Link>
             </div>
           </div>
 
-          <div className="px-6 pt-1 pb-5">
-            <Link
-              href="/car-rental?product=halo#form-rental"
-              onClick={hrefTransition}
-              legacyBehavior
-            >
-              <a className="bg-teal-500 text-white font-bold py-2 px-4 rounded-lg">
-                Sewa Armada
-              </a>
-            </Link>
+          <div className="rounded-lg overflow-hidden shadow-md">
+            <img
+              className="w-full h-[350px] object-cover object-center"
+              src="https://images.unsplash.com/photo-1552374196-1ab2a1c593e8?auto=format&q=75&fit=crop&crop=top&w=600&h=700"
+              loading="lazy"
+              alt="Sunset in the mountains"
+            />
+
+            <div className="px-6 py-4">
+              <h2 className="font-bold text-xl text-primary">AVANZA</h2>
+
+              <p className="font-semibold text-lg text-gray-500">
+                IDR 150.000/hari
+              </p>
+
+              <div className="pt-3">
+                <div className="flex items-center pb-1">
+                  <span className="mr-2 text-lg text-teal-500">
+                    <FaCheckCircle />
+                  </span>
+                  <p className="font-sans text-sm text-[#4F4F4F]">Driver</p>
+                </div>
+
+                <div className="flex items-center pb-1">
+                  <span className="mr-2 text-lg text-teal-500">
+                    <FaCheckCircle />
+                  </span>
+                  <p className="font-sans text-sm text-[#4F4F4F]">BBM</p>
+                </div>
+
+                <div className="flex items-center">
+                  <span className="mr-2 text-lg text-teal-500">
+                    <FaCheckCircle />
+                  </span>
+                  <p className="font-sans text-sm text-[#4F4F4F]">
+                    6-7 Penumpang
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="px-6 pt-1 pb-5">
+              <Link
+                href="/car-rental?product=halo#form-rental"
+                onClick={hrefTransition}
+                legacyBehavior
+              >
+                <a className="bg-teal-500 text-white font-bold py-2 px-4 rounded-lg">
+                  Sewa Armada
+                </a>
+              </Link>
+            </div>
           </div>
         </div>
       </section>
