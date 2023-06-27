675f90943053a51ca70473b10849ab8560396dac
dany-boop <dny77853@gmail.com>
2023-06-27 18:50:27 +0700

    ...

--- src/components/Containers/Gallery/Gallery.tsx
+++ src/components/Containers/Gallery/Gallery.tsx
@@ -13,75 +13,75 @@ const ContainerGallery: FC = () => {
         <div className="max-w-full md:max-w-7xl mx-auto">
           {/* gallery start */}
           <div className="min-h-screen bg-white py-10">
-            <div className="over container mx-auto w-full  md:max-w-7xl px-10 xl:px-0">
-              <div className="grid grid-cols-1 gap-5 md:bmc rounded md:border bg-white">
-                <div className="border ">
+            <div className="over container mx-auto w-full md:max-w-7xl px-10 lg:px-0">
+              <div className="grid md:grid-cols-2 lg:grid-cols-3 rounded md:border bg-white">
+                <div className="border grid row-span-2 overflow-hidden">
                   <img
-                    className="w-full h-full bg-cover bg-center"
+                    className="w-full h-full bg-cover bg-center hover:scale-105 inset-0 transition duration-200"
                     src="assets/img/Bromo 1.JPG"
                     alt="BromoImage"
                   />
                 </div>
 
-                <div className="border">
+                <div className="border overflow-hidden">
                   <img
-                    className="w-full h-full bg-cover bg-center"
+                    className="w-full h-full bg-cover bg-center hover:scale-105 inset-0 transition duration-200"
                     src="assets/img/Bromo 1.JPG"
                     alt="BromoImage"
                   />
                 </div>
 
-                <div className="border ">
+                <div className="border overflow-hidden ">
                   <img
-                    className="w-full h-full bg-cover bg-center"
+                    className="w-full h-full bg-cover bg-center hover:scale-105 inset-0 transition duration-200"
                     src="assets/img/Bromo 1.JPG"
                     alt="BromoImage"
                   />
                 </div>
 
-                <div className="border ">
+                <div className="border overflow-hidden grid row-span-2 ">
                   <img
-                    className="w-full h-full bg-cover bg-center"
+                    className="w-full h-full bg-cover bg-center hover:scale-105 inset-0 transition duration-200"
                     src="assets/img/Bromosunset.JPG"
                     alt="BromoImage"
                   />
                 </div>
 
-                <div className="border ">
+                <div className="border overflow-hidden ">
                   <img
-                    className="w-full h-full bg-cover bg-center"
+                    className="w-full h-full bg-cover bg-center hover:scale-105 inset-0 transition duration-200"
                     src="assets/img/Bromo 1.JPG"
                     alt="BromoImage"
                   />
                 </div>
 
-                <div className="border ">
+                <div className="border overflow-hidden ">
                   <img
-                    className="w-full h-full bg-cover bg-center"
+                    className="w-full h-full bg-cover bg-center hover:scale-105 inset-0 transition duration-200"
                     src="assets/img/Bromo 1.JPG"
                     alt="BromoImage"
                   />
                 </div>
 
-                <div className="border ">
+                <div className="border overflow-hidden grid row-span-2 ">
                   <img
-                    className="w-full h-full bg-cover bg-center"
+                    className="w-full h-full bg-cover bg-center hover:scale-105 inset-0 transition duration-200"
                     src="assets/img/Bromo 1.JPG"
                     alt="BromoImage"
                   />
                 </div>
 
-                <div className="border ">
+                <div className="border overflow-hidden ">
                   <img
-                    className="w-full h-full bg-cover bg-center"
+                    className="w-full h-full bg-cover bg-center hover:scale-105 inset-0 transition duration-200"
                     src="assets/img/Bromo 1.JPG"
                     alt="BromoImage"
                   />
                 </div>
 
-                <div className="border ">
+                <div className="border overflow-hidden ">
                   <img
-                    className="w-full h-full bg-cover bg-center"
+                    className="w-full h-full bg-cover bg-center hover:scale-105 inset-0 transition duration-200"
                     src="assets/img/Bromo 1.JPG"
                     alt="BromoImage"
                   />
