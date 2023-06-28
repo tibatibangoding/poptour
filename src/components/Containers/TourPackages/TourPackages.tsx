3f27622c5e31ca0fd799eafe48dab2820472af53
aryafirmansyah2 <aryafirmansyah200404@example.com>
2023-06-28 20:30:23 +0700

    add button day

--- src/components/Containers/TourPackages/TourPackages.tsx
+++ src/components/Containers/TourPackages/TourPackages.tsx
@@ -13,9 +13,30 @@ const ConatinerTourPackages = () => {
 
       <div className="py-20">
         <div className="mx-10 md:mx-24 lg:mx-40 py-20 ">
-          <div className='flex gap-5'>
-            <div className='px-3 bg-[#f6f6f6]'>
-              <h1>1 Day</h1>
+          <div className='flex gap-5 mb-10'>
+            <div className='px-5 py-2 rounded-full bg-[#f6f6f6] border-[1px] border-[#ccc]'>
+              <h1 className='text-[#666]'>1 Day</h1>
+            </div>
+            <div className='px-5 py-2 rounded-full bg-[#f6f6f6] border-[1px] border-[#ccc] '>
+              <h1 className='text-[#666]'>2 Day</h1>
+            </div>
+            <div className='px-5 py-2 rounded-full bg-[#f6f6f6] border-[1px] border-[#ccc]'>
+              <h1 className='text-[#666]'>3 Day</h1>
+            </div>
+            <div className='px-5 py-2 rounded-full bg-[#f6f6f6] border-[1px] border-[#ccc]'>
+              <h1 className='text-[#666]'>4 Day</h1>
+            </div>
+            <div className='px-5 py-2 rounded-full bg-[#f6f6f6] border-[1px] border-[#ccc]'>
+              <h1 className='text-[#666]'>5 Day</h1>
+            </div>
+            <div className='px-5 py-2 rounded-full bg-[#f6f6f6] border-[1px] border-[#ccc]'>
+              <h1 className='text-[#666]'>6 Day</h1>
+            </div>
+            <div className='px-5 py-2 rounded-full bg-[#f6f6f6] border-[1px] border-[#ccc]'>
+              <h1 className='text-[#666]'>Haneymoon</h1>
+            </div>
+            <div className='px-5 py-2 rounded-full bg-[#f6f6f6] border-[1px] border-[#ccc]'>
+              <h1 className='text-[#666]'>Gath Perusahaan</h1>
             </div>
 
           </div>
