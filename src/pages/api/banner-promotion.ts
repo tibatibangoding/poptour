2fdf57a0662b4ecd43c56573e176735c5f7881a2
Naufal Akbar <kbrnugroho@gmail.com>
2023-06-30 15:27:23 +0700

    ...

--- src/pages/api/banner-promotion.ts
+++ src/pages/api/banner-promotion.ts
@@ -14,16 +14,46 @@ export default function handler(
   if (method === 'GET') {
     return res.status(200).json({
       banner: [
-        { img_src: 'assets/img/poster/1.png', title: 'poster' },
-        { img_src: 'assets/img/poster/2.png', title: 'poster' },
-        { img_src: 'assets/img/poster/3.png', title: 'poster' },
-        { img_src: 'assets/img/poster/4.png', title: 'poster' },
-        { img_src: 'assets/img/poster/5.png', title: 'poster' },
-        { img_src: 'assets/img/poster/6.png', title: 'poster' },
-        { img_src: 'assets/img/poster/7.png', title: 'poster' },
-        { img_src: 'assets/img/poster/9.png', title: 'poster' },
-        { img_src: 'assets/img/poster/10.jpeg', title: 'poster' },
-        { img_src: 'assets/img/poster/11.jpeg', title: 'poster' },
+        {
+          img_src: 'https://poptour.vercel.app/assets/img/poster/1.png',
+          title: 'Banner Promotion',
+        },
+        {
+          img_src: 'https://poptour.vercel.app/assets/img/poster/2.png',
+          title: 'Banner Promotion',
+        },
+        {
+          img_src: 'https://poptour.vercel.app/assets/img/poster/3.png',
+          title: 'Banner Promotion',
+        },
+        {
+          img_src: 'https://poptour.vercel.app/assets/img/poster/4.png',
+          title: 'Banner Promotion',
+        },
+        {
+          img_src: 'https://poptour.vercel.app/assets/img/poster/5.png',
+          title: 'Banner Promotion',
+        },
+        {
+          img_src: 'https://poptour.vercel.app/assets/img/poster/6.png',
+          title: 'Banner Promotion',
+        },
+        {
+          img_src: 'https://poptour.vercel.app/assets/img/poster/7.png',
+          title: 'Banner Promotion',
+        },
+        {
+          img_src: 'https://poptour.vercel.app/assets/img/poster/9.png',
+          title: 'Banner Promotion',
+        },
+        {
+          img_src: 'https://poptour.vercel.app/assets/img/poster/10.jpeg',
+          title: 'Banner Promotion',
+        },
+        {
+          img_src: 'https://poptour.vercel.app/assets/img/poster/11.jpeg',
+          title: 'Banner Promotion',
+        },
       ],
     });
   }
