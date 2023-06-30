9a36515e591a19d75b3c940923f1a146220de755
Naufal Akbar <kbrnugroho@gmail.com>
2023-06-30 19:20:31 +0700

    try fix error

--- src/pages/tour-packages/detail/[slug].tsx
+++ src/pages/tour-packages/detail/[slug].tsx
@@ -8,19 +8,19 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tour-packages`);
   const data = await res.json();
 
-  const paths = data.tour.map((item: TourPackages) => ({
-    params: { slug: item.slug },
+  const paths = data?.tour?.map((item: TourPackages) => ({
+    params: { slug: item?.slug },
   }));
 
   return { paths, fallback: false };
 };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+export const getStaticProps: GetStaticProps = async ({ params }: any) => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tour-packages`);
   const resJson = await res.json();
 
   const data = resJson.tour.filter(
-    (item: TourPackages) => item.slug === params?.slug
+    (item: TourPackages) => item?.slug === params?.slug
   );
 
   return {
