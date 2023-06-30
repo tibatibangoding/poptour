7b0d77ec91af30a71378518a2d111c9ba6bffc1f
Naufal Akbar <kbrnugroho@gmail.com>
2023-06-30 13:37:04 +0700

    create api

---
+++ src/pages/api/tour-packages.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { TourPackages } from '../../interfaces/tourPackages';
+
+type Data = {
+  tour: TourPackages[];
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({
+    tour: [
+      {
+        address: 'Bromo, Malang Jawa Timur',
+        tags: [{ tag: 'All in' }, { tag: 'All in' }],
+        title: 'Paket Wisata Bromo',
+        day: 4,
+        airport: 'Soekarno Hatta',
+        price: '5.000.000',
+        href: '/tour-packages/detail/paket-wisata-bromo',
+      },
+      {
+        address: 'Bromo, Malang Jawa Timur',
+        tags: [{ tag: 'All in' }, { tag: 'All in' }],
+        title: 'Paket Wisata Pantai Sendang Biru',
+        day: 4,
+        airport: 'Soekarno Hatta',
+        price: '5.000.000',
+        href: '/tour-packages/detail/paket-wisata-pantai-sendang-biru',
+      },
+      {
+        address: 'Bromo, Malang Jawa Timur',
+        tags: [{ tag: 'All in' }, { tag: 'All in' }],
+        title: 'Paket Wisata Pantai Balekambang',
+        day: 4,
+        airport: 'Soekarno Hatta',
+        price: '5.000.000',
+        href: '/tour-packages/detail/paket-wisata-pantai-balekambang',
+      },
+    ],
+  });
+}
