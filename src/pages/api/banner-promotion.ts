37fa26b526ac2ee13ea5bf89b7fc4263437d8483
Naufal Akbar <kbrnugroho@gmail.com>
2023-06-30 14:09:21 +0700

    trying fix error cors

--- src/pages/api/banner-promotion.ts
+++ src/pages/api/banner-promotion.ts
@@ -9,18 +9,22 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({
-    banner: [
-      { img_src: 'assets/img/poster/1.png', title: 'poster' },
-      { img_src: 'assets/img/poster/2.png', title: 'poster' },
-      { img_src: 'assets/img/poster/3.png', title: 'poster' },
-      { img_src: 'assets/img/poster/4.png', title: 'poster' },
-      { img_src: 'assets/img/poster/5.png', title: 'poster' },
-      { img_src: 'assets/img/poster/6.png', title: 'poster' },
-      { img_src: 'assets/img/poster/7.png', title: 'poster' },
-      { img_src: 'assets/img/poster/9.png', title: 'poster' },
-      { img_src: 'assets/img/poster/10.jpeg', title: 'poster' },
-      { img_src: 'assets/img/poster/11.jpeg', title: 'poster' },
-    ],
-  });
+  const { method } = req;
+
+  if (method === 'GET') {
+    return res.status(200).json({
+      banner: [
+        { img_src: 'assets/img/poster/1.png', title: 'poster' },
+        { img_src: 'assets/img/poster/2.png', title: 'poster' },
+        { img_src: 'assets/img/poster/3.png', title: 'poster' },
+        { img_src: 'assets/img/poster/4.png', title: 'poster' },
+        { img_src: 'assets/img/poster/5.png', title: 'poster' },
+        { img_src: 'assets/img/poster/6.png', title: 'poster' },
+        { img_src: 'assets/img/poster/7.png', title: 'poster' },
+        { img_src: 'assets/img/poster/9.png', title: 'poster' },
+        { img_src: 'assets/img/poster/10.jpeg', title: 'poster' },
+        { img_src: 'assets/img/poster/11.jpeg', title: 'poster' },
+      ],
+    });
+  }
 }
--- src/pages/api/tour-packages.ts
+++ src/pages/api/tour-packages.ts
@@ -9,35 +9,39 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({
-    tour: [
-      {
-        address: 'Bromo, Malang Jawa Timur',
-        tags: [{ tag: 'All in' }, { tag: 'All in' }],
-        title: 'Paket Wisata Bromo',
-        day: 4,
-        airport: 'Soekarno Hatta',
-        price: '5.000.000',
-        href: '/tour-packages/detail/paket-wisata-bromo',
-      },
-      {
-        address: 'Bromo, Malang Jawa Timur',
-        tags: [{ tag: 'All in' }, { tag: 'All in' }],
-        title: 'Paket Wisata Pantai Sendang Biru',
-        day: 4,
-        airport: 'Soekarno Hatta',
-        price: '5.000.000',
-        href: '/tour-packages/detail/paket-wisata-pantai-sendang-biru',
-      },
-      {
-        address: 'Bromo, Malang Jawa Timur',
-        tags: [{ tag: 'All in' }, { tag: 'All in' }],
-        title: 'Paket Wisata Pantai Balekambang',
-        day: 4,
-        airport: 'Soekarno Hatta',
-        price: '5.000.000',
-        href: '/tour-packages/detail/paket-wisata-pantai-balekambang',
-      },
-    ],
-  });
+  const { method } = req;
+
+  if (method === 'GET') {
+    res.status(200).json({
+      tour: [
+        {
+          address: 'Bromo, Malang Jawa Timur',
+          tags: [{ tag: 'All in' }, { tag: 'All in' }],
+          title: 'Paket Wisata Bromo',
+          day: 4,
+          airport: 'Soekarno Hatta',
+          price: '5.000.000',
+          href: '/tour-packages/detail/paket-wisata-bromo',
+        },
+        {
+          address: 'Bromo, Malang Jawa Timur',
+          tags: [{ tag: 'All in' }, { tag: 'All in' }],
+          title: 'Paket Wisata Pantai Sendang Biru',
+          day: 4,
+          airport: 'Soekarno Hatta',
+          price: '5.000.000',
+          href: '/tour-packages/detail/paket-wisata-pantai-sendang-biru',
+        },
+        {
+          address: 'Bromo, Malang Jawa Timur',
+          tags: [{ tag: 'All in' }, { tag: 'All in' }],
+          title: 'Paket Wisata Pantai Balekambang',
+          day: 4,
+          airport: 'Soekarno Hatta',
+          price: '5.000.000',
+          href: '/tour-packages/detail/paket-wisata-pantai-balekambang',
+        },
+      ],
+    });
+  }
 }
