87459b3e0953e4fa57469f3271017bdcb32f968c
Naufal Akbar <kbrnugroho@gmail.com>
2023-06-30 16:04:04 +0700

    add shadow and delete console.log

--- src/components/Common/BannerPromotion.tsx
+++ src/components/Common/BannerPromotion.tsx
@@ -12,37 +12,37 @@ import { BannerPromotion } from '@/interfaces/bannerPromotion';
 const BannerPromotion: FC = () => {
   const { banner } = useBanner();
 
-  console.log(banner);
-
   return (
-    <Swiper
-      navigation={false}
-      pagination={{
-        clickable: true,
-      }}
-      centeredSlides={true}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }}
-      modules={[Autoplay, Navigation, Pagination]}
-      className="mySwiper"
-    >
-      {banner?.map((item: BannerPromotion, index: number) => (
-        <SwiperSlide key={index}>
-          <section className="w-full h-[200px] md:h-[300px] lg:h-[400px]">
-            <div className="w-full h-full">
-              <img
-                src={item.img_src}
-                alt={item.title}
-                loading="lazy"
-                className="object-cover object-left w-full h-[200px] md:h-[300px] lg:h-[400px]"
-              />
-            </div>
-          </section>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <section className="shadow-md">
+      <Swiper
+        navigation={false}
+        pagination={{
+          clickable: true,
+        }}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        {banner?.map((item: BannerPromotion, index: number) => (
+          <SwiperSlide key={index}>
+            <section className="w-full h-[200px] md:h-[300px] lg:h-[400px]">
+              <div className="w-full h-full">
+                <img
+                  src={item.img_src}
+                  alt={item.title}
+                  loading="lazy"
+                  className="object-cover object-left w-full h-[200px] md:h-[300px] lg:h-[400px]"
+                />
+              </div>
+            </section>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
   );
 };
 
