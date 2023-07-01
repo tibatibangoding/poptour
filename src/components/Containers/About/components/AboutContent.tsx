ef38052025fddf72c0f38936d68fb79bdc2878df
Naufal Akbar <kbrnugroho@gmail.com>
2023-07-01 11:02:16 +0700

    revamp about page

--- src/components/Containers/About/components/AboutContent.tsx
+++ src/components/Containers/About/components/AboutContent.tsx
@@ -2,51 +2,58 @@ import { FC } from 'react';
 
 const AboutContent: FC = () => {
   return (
-    <div className="justify-center items-center mt-5 mb-5 md:mt-10 md:mb-10 ">
-      <h1 className="font-bold text-2xl md:text-3xl text-center capitalize">
-        Tentang Kami
-      </h1>
+    <div className="container py-10 lg:py-24">
+      <div className="flex flex-wrap justify-center items-center">
+        <div className="flex flex-col items-start lg:items-center justify-start lg:justify-center px-4">
+          <p className="font-primary font-semibold text-md lg:text-lg text-primary uppercase lg:mb-2">
+            Tentang Kami
+          </p>
 
-      <img
-        className="w-20 h-20 mx-auto my-10"
-        src="/assets/img/logo/POPTour_4.png"
-        alt="Logo"
-      />
+          <img
+            src="/assets/img/logo/PopTour_Hori_Black.png"
+            alt="Brand Logo"
+            className="w-96 h-40 mx-auto object-cover object-center"
+          />
+        </div>
 
-      <h2 className="font-medium text-lg">Selamat datang di Pop Tour,</h2>
+        <div>
+          <h2 className="font-medium text-lg">Selamat datang di Pop Tour,</h2>
 
-      <p className="w-full pt-5 mx-auto">
-        Kami adalah sahabat wisata dan perjalanan Anda. Pop Tour telah
-        berpengalaman dalam perencanaan perjalanan wisata ke berbagai destinasi
-        di Kota Malang. Bersama Pop Tour, Kami membantu anda untuk mengeksplor
-        dan menjelajahi budaya baru, bersantai di pantai, menikmati keindahan
-        gunung atau memulai petualangan yang belum pernah anda rasakan
-        sebelumnya. Kami bangga dengan perhatian yang kami berikan pada detail
-        dan layanan untuk menjamin bahwa setiap perjalanan Anda akan menjadi
-        menyenangkan dan berjalan sesuai dengan keinginan.
-      </p>
+          <p className="w-full pt-5 mx-auto">
+            Kami adalah sahabat wisata dan perjalanan Anda. Pop Tour telah
+            berpengalaman dalam perencanaan perjalanan wisata ke berbagai
+            destinasi di Kota Malang. Bersama Pop Tour, Kami membantu anda untuk
+            mengeksplor dan menjelajahi budaya baru, bersantai di pantai,
+            menikmati keindahan gunung atau memulai petualangan yang belum
+            pernah anda rasakan sebelumnya. Kami bangga dengan perhatian yang
+            kami berikan pada detail dan layanan untuk menjamin bahwa setiap
+            perjalanan Anda akan menjadi menyenangkan dan berjalan sesuai dengan
+            keinginan.
+          </p>
 
-      <br />
+          <br />
 
-      <p>Adapun pelayanan yang kami tawarkan adalah:</p>
+          <p>Adapun pelayanan yang kami tawarkan adalah:</p>
 
-      <br />
+          <br />
 
-      <ul className="list-disc ml-10 sm:ml-32 ">
-        <li>Paket Wisata / Study Banding</li>
-        <li>
-          Paket Outbound, Team Building, Adventure, Fun Games & Fun Off Road
-        </li>
-        <li>Rental Mobil, Elf, Hiace & Bus Pariwisata</li>
-        <li>Reservasi Hotel</li>
-      </ul>
+          <ul className="list-disc ml-10 sm:ml-32 ">
+            <li>Paket Wisata / Study Banding</li>
+            <li>
+              Paket Outbound, Team Building, Adventure, Fun Games & Fun Off Road
+            </li>
+            <li>Rental Mobil, Elf, Hiace & Bus Pariwisata</li>
+            <li>Reservasi Hotel</li>
+          </ul>
 
-      <br />
+          <br />
 
-      <p>
-        Didukung oleh staf kami yang berpengalaman dibidangnya, kami yakin bahwa
-        kami bisa membangun hubungan bisnis yang baik.
-      </p>
+          <p>
+            Didukung oleh staf kami yang berpengalaman dibidangnya, kami yakin
+            bahwa kami bisa membangun hubungan bisnis yang baik.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
