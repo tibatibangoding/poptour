c5c88b10a278fae04cd013c5681b4b638d047b10
Naufal Akbar <kbrnugroho@gmail.com>
2023-07-02 13:48:31 +0700

    activate form

--- src/components/Containers/Contact/Components/Form.tsx
+++ src/components/Containers/Contact/Components/Form.tsx
@@ -1,25 +1,90 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+
+import { FormContact } from '@/interfaces/formContact';
+import { useAxios } from '@/hooks/useAxios';
+import { errorToast, successToast } from '@/lib/toastNotify';
 
 const Form: FC = () => {
+  const [formData, setFormData] = useState<FormContact>({
+    fullName: '',
+    email: '',
+    noWa: '',
+    typeForm: '',
+    notes: '',
+  });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const axios = useAxios();
+
+  const handleChange = (e: any) => {
+    setFormData((prevalue) => {
+      return {
+        ...prevalue,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const { status } = await axios.post('contact', formData);
+
+      if (status === 200) {
+        setIsLoading(false);
+        setFormData({
+          fullName: '',
+          email: '',
+          noWa: '',
+          typeForm: '',
+          notes: '',
+        });
+        successToast('Kritik dan Saran berhasil dikirim!');
+      }
+    } catch (err) {
+      setFormData({
+        fullName: '',
+        email: '',
+        noWa: '',
+        typeForm: '',
+        notes: '',
+      });
+      errorToast('Oops! Terjadi kesalahan, silahkan coba lagi!');
+    } finally {
+      setIsLoading(false);
+      setFormData({
+        fullName: '',
+        email: '',
+        noWa: '',
+        typeForm: '',
+        notes: '',
+      });
+    }
+  };
+
   return (
-    <div className="container py-10 lg:py-24">
+    <div className="container mx-auto py-10 lg:py-24">
       <div className="flex flex-wrap">
         <div className="mb-10 lg:mb-0 w-full px-4 lg:w-2/3">
           <h1 className="font-primary font-semibold text-2xl lg:text-3xl mb-2">
             Kritik dan Saran
           </h1>
 
-          <form className="my-5">
+          <form className="my-5" onSubmit={handleSubmit}>
             <div className="flex flex-wrap mb-2">
               <div className="w-full lg:pr-4 lg:w-1/2">
                 <label
                   htmlFor="fullName"
                   className="text-gray-500 xl:text-lg leading-relaxed"
                 >
-                  Nama Lengkap
+                  Nama Lengkap <span className="text-red-500">*</span>
                 </label>
 
                 <input
+                  onChange={handleChange}
+                  value={formData.fullName}
                   type="text"
                   id="fullName"
                   name="fullName"
@@ -34,10 +99,12 @@ const Form: FC = () => {
                   htmlFor="email"
                   className="text-gray-500 xl:text-lg leading-relaxed"
                 >
-                  Email
+                  Email <span className="text-red-500">*</span>
                 </label>
 
                 <input
+                  onChange={handleChange}
+                  value={formData.email}
                   type="email"
                   id="email"
                   name="email"
@@ -50,16 +117,18 @@ const Form: FC = () => {
 
             <div className="mb-2">
               <label
-                htmlFor="tel"
+                htmlFor="noWa"
                 className="text-gray-500 xl:text-lg leading-relaxed"
               >
-                No. Telepon
+                No. Telepon <span className="text-red-500">*</span>
               </label>
 
               <input
+                onChange={handleChange}
+                value={formData.noWa}
                 type="text"
-                id="tel"
-                name="tel"
+                id="noWa"
+                name="noWa"
                 placeholder="Masukkan No. Telepon"
                 className="w-full border border-gray-300 rounded p-2 focus:outline-none focus:ring focus:ring-primary/70 my-2"
                 required
@@ -67,17 +136,43 @@ const Form: FC = () => {
             </div>
 
             <div className="mb-2">
+              <label
+                htmlFor="typeForm"
+                className="text-gray-500 xl:text-lg leading-relaxed"
+              >
+                Jenis Formulir <span className="text-red-500">*</span>
+              </label>
+
+              <select
+                onChange={handleChange}
+                value={formData.typeForm || ''}
+                name="typeForm"
+                id="typeForm"
+                className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring my-2"
+                required
+              >
+                <option defaultValue="Pilih Jenis Formulir" selected disabled>
+                  -- Pilih Jenis Formulir --
+                </option>
+                <option value="Kritik">Kritik</option>
+                <option value="Saran">Saran</option>
+              </select>
+            </div>
+
+            <div className="mb-2">
               <div className="flex flex-col">
                 <label
-                  htmlFor="message"
+                  htmlFor="notes"
                   className="text-gray-500 xl:text-lg leading-relaxed"
                 >
-                  Kritik dan Saran
+                  Kritik dan Saran <span className="text-red-500">*</span>
                 </label>
 
                 <textarea
-                  name="message"
-                  id="message"
+                  onChange={handleChange}
+                  value={formData.notes}
+                  name="notes"
+                  id="notes"
                   cols={8}
                   rows={8}
                   placeholder="Masukkan Kritik dan Saran"
@@ -89,11 +184,16 @@ const Form: FC = () => {
             <div className="mb-2">
               <button
                 type="submit"
-                className="w-full bg-primary hover:bg-secondary active:bg-secondary/70 focus-visible:ring ring-primary/30 text-white hover:text-primary text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-200 px-5 py-3"
+                className={`${
+                  isLoading ? 'cursor-not-allowed' : 'cursor-pointer'
+                } w-full bg-primary hover:bg-secondary active:bg-secondary/70 focus-visible:ring ring-primary/30 text-white hover:text-primary text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-200 px-5 py-3`}
+                disabled={isLoading ? true : false}
               >
-                Kirim
+                {isLoading ? 'Memuat...' : 'Kirim'}
               </button>
             </div>
+
+            <span className="text-sm text-red-500">*Wajib</span>
           </form>
         </div>
 
