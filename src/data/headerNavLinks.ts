dbec1f932ff92002e79c250b90a59b8192e9e500
Naufal Akbar <kbrnugroho@gmail.com>
2023-07-06 14:27:29 +0700

    update

--- src/data/headerNavLinks.ts
+++ src/data/headerNavLinks.ts
@@ -3,7 +3,7 @@ export const headerNavLinks = [
   { title: 'Paket Wisata', path: '/tour-packages' },
   { title: 'Rental Mobil', path: '/car-rental' },
   { title: 'Galeri', path: '/gallery' },
+  { title: 'Ulasan', path: '/reviews' },
   { title: 'Tentang', path: '/about-us' },
-  { title: 'Reviews', path: '/reviews' },
   { title: 'Kontak', path: '/contact' },
 ];
